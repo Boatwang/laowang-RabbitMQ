4da39abaeafcaf35e71af6eac5f95014d157b2ae
laowang <laowang@163.com>
2022-05-16 20:13:23 +0800

    暂时完成

--- rabbitmq-hello/src/main/java/com/laowang/rabbitmq/five/EmitLog.java
+++ rabbitmq-hello/src/main/java/com/laowang/rabbitmq/five/EmitLog.java
@@ -1,9 +1,31 @@
 package com.laowang.rabbitmq.five;
 
+import com.laowang.rabbitmq.utils.RabbitMqUtils;
+import com.rabbitmq.client.Channel;
+
+import java.nio.charset.StandardCharsets;
+import java.util.Scanner;
+
 /**
  * @CreateTime 2022/5/16-16 18:01
  * @Author laowang
  * @Description 发消息给交换机
  */
 public class EmitLog {
+
+    public static final String EXHANGE_NAME = "logs";
+
+    public static void main(String[] args) throws Exception{
+        Channel channel = RabbitMqUtils.getChannel();
+
+        channel.exchangeDeclare(EXHANGE_NAME,"fanout");
+
+        Scanner scanner = new Scanner(System.in);
+
+        while (scanner.hasNext()){
+            String message = scanner.next();
+            channel.basicPublish(EXHANGE_NAME,"",null,message.getBytes(StandardCharsets.UTF_8));
+            System.out.println("生产者发出消息："+message);
+        }
+    }
 }
